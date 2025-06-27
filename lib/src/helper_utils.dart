7d0b2e6ddceb3e0632343cbd9aafe7a561ce2c43
Yoeri <yoeriz123@gmail.com>
2025-06-27 14:01:29 +0200

    made method static

--- lib/src/helper_utils.dart
+++ lib/src/helper_utils.dart
@@ -130,7 +130,7 @@ abstract final class HelperUtils {
   }
 
   ///Gets the names of files from their urls, this is simply the last path segment
-  List<String> getNamesFromUrls(List<String> fileUrls) {
+  static List<String> getNamesFromUrls(List<String> fileUrls) {
     return fileUrls.map((f) => Uri.parse(f).pathSegments.last).toList();
   }
 }
